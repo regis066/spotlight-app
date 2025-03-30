7ce3e5482d6a80f4059020da9ab566c2462966be
regis066 <mugisharegis72@gmail.com>
2025-03-30 18:00:35 +0200

    refactor: utilize InitialLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
+import InitialLayout from "@/components/initialLayout";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -15,14 +15,7 @@ export default function RootLayout() {
     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1 }}>
-          <Stack>
-            <Stack.Screen
-              name="(tabs)"
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack>
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
